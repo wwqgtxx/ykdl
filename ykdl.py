7360df44fba8510b1785dff2ae80b00935924c74
wwqgtxx <wwqgtxx@gmail.com>
2018-03-30 10:21:55 +0800

    update for wwqLyParse
    Signed-off-by: wwqgtxx <wwqgtxx@gmail.com>

--- ykdl.py
+++ ykdl.py
@@ -5,9 +5,11 @@ try:
     import ykdl
 except(ImportError):
     import sys
+    import os
     _base_len = len('bin/ykdl')
     _filepath = sys.argv[0][:-_base_len]
     sys.path.insert(1, _filepath)
+    sys.path.insert(1, os.path.join(_filepath, '../lib/requests_lib/'))
     import ykdl
     
 from cykdl.__main__ import main
