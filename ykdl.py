c4700128da13e6f689f0391f55136637c840a9e4
wwqgtxx <wwqgtxx@gmail.com>
2018-07-13 13:06:42 +0800

    patch html.get_location in ykdl too

--- ykdl.py
+++ ykdl.py
@@ -13,11 +13,10 @@ except(ImportError):
     sys.path.insert(1, os.path.join(_filepath, '../lib/requests_lib/'))
     import ykdl
 
-conn = None
-
 import json
 import socket
 import traceback
+import base64
 from argparse import ArgumentParser
 from multiprocessing.connection import Client, Connection
 
@@ -28,28 +27,6 @@ logger = logging.getLogger("YKDL")
 from ykdl.common import url_to_module
 from ykdl.version import __version__
 from ykdl.util import html
-
-_get_content = html.get_content
-
-
-def get_content(url, headers=html.fake_headers, data=None, charset=None):
-    # logger.warning((url, data, charset, headers))
-    if conn:
-        bd = json.dumps({"url": url, "headers": headers, "data": data, "charset": charset}).encode("utf-8")
-        try:
-            conn.send_bytes(bd)
-            resp = conn.recv_bytes()
-            if charset == "ignore":
-                return resp
-            else:
-                return resp.decode("utf-8")
-        except Exception:
-            logger.exception("get_content")
-            sys.exit(0)
-    return _get_content(url, headers, data, charset)
-
-
-html.get_content = get_content
 
 
 def arg_parser():
@@ -70,8 +47,47 @@ def main():
     args = arg_parser()
     address = os.environ.get("address_get_url", None)
     if address:
-        global conn
         conn = Client(address=address)
+
+        def _get(bd, decoded=True):
+            try:
+                conn.send_bytes(bd)
+                resp = conn.recv_bytes()
+                if decoded:
+                    return resp.decode("utf-8")
+                else:
+                    return resp
+            except Exception:
+                sys.exit(0)
+
+        class Response(object):
+            __slots__ = ("data", "headers", "url")
+
+        def get_response(url, headers=None, get_method=None, no_logging=False):
+            if not no_logging:
+                logging.debug('get_response: %s' % url)
+
+            bd = json.dumps({"url": url, "headers": headers,
+                             "encoding": "response", "method": get_method}).encode("utf-8")
+            resp = json.loads(_get(bd, True))
+            response = Response()
+            response.data = base64.b64decode(resp["data"])
+            response.headers = resp["headers"]
+            response.url = resp["url"]
+            return response
+
+        def get_location(url, headers=html.fake_headers):
+            resp = get_response(url, headers=headers, no_logging=True)
+            return resp.url
+
+        def get_content(url, headers=html.fake_headers, data=None, charset=None):
+            # logger.warning((url, data, charset, headers))
+            bd = json.dumps({"url": url, "headers": headers, "data": data, "charset": charset}).encode("utf-8")
+            return _get(bd, charset != "ignore")
+
+        html.get_location = get_location
+        html.get_content = get_content
+
     logging.root.setLevel(logging.WARNING)
     # if not args.debug:
     #     logging.root.setLevel(logging.WARNING)
